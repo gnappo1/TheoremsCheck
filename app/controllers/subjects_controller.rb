8b9f735b36f73b439b9fc158406dce75758386f5
Matteo Piccini <gnappo.1@live.it>
2017-10-25 12:08:56 +0000

    Defined remaining actions in SubjectController

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,6 @@
 class SubjectsController < ApplicationController
   before_action :set_subject, only: [:show, :edit, :update, :destroy]
-  before_action :check_admin, only: [:edit, :update, :destroy]
+  before_action :check_admin, only: [:edit, :destroy]
 
   def index
     @subjects = Subject.all
@@ -9,7 +9,34 @@ class SubjectsController < ApplicationController
   def show
   end
 
-  
+  def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      redirect_to subjects_path, notice: 'Subject was successfully created.'
+    else
+      render 'new', notice: 'Oops! There was a problem creating the Subject.'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @subject.update(subject_params)
+      redirect_to @subject, notice: 'Subject was successfully updated!'
+    else
+      render 'edit', notice: 'Oops! There was a problem updating the Subject.'
+    end
+  end
+
+  def destroy
+    @subject.destroy
+  end
+
   private
 
   def set_subject
